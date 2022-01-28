1ef786f329a275dfbe9f52209c9c0d0b0fb9e73a
Yuliia Besedina <92574244+bjula@users.noreply.github.com>
2022-01-28 18:38:05 +0200

    add task 6-8/36

--- js/script.js
+++ js/script.js
@@ -31,4 +31,33 @@
 // const message = `You picked ${productName}, price per item is ${pricePerItem} credits.`
 // console.log(message); //You picked Droid, price per item is 3500 credits.
 
-// blok - 1; task 6/36:
+// // blok - 1; task 6/36:
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`
+// console.log(message); //You ordered droids worth 4850 credits. Delivery (50 credits) is included in total price.
+
+// // blok - 1; task 7/36: Функция:
+// function sayHi(){console.log("Hello, this is my first function!")};
+// sayHi();// Hello, this is my first function!
+
+// // blok - 1; task 8/36:
+// function add(a, b, c) {
+//   console.log(`Addition result equals ${a+b+c}`);
+// }
+
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
+
+// blok - 1; task 9/36:
+
+
+
+
+
+
+
+
