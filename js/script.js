9fba8a8e947d764ee25749fe93c83bec45a8ce29
Yuliia Besedina <92574244+bjula@users.noreply.github.com>
2022-01-28 15:08:48 +0200

    Add prettierrc and script .  1 part example task js

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// const yearOfBirth = 2006;
+
+// // Если переменная объявлена как const, перезаписать ее значение нельзя.
+// // При попытке присвоить новое значение будешь ошибка выполнения скрипта.
+// // yearOfBirth = 2020; // ❌ Неправильно, будет ошибка
+
+// console.log(yearOfBirth);
+
+// let age;
+
+// age = 15;
+
+// console.log(age);  // Если переменной объявленной как let изначально не присвоено значение,
+// // она инициализируется специальным значением undefined (не определено).  //undefined
+
+// blok - 1; task 4/36:
+// let productName = "Droid";
+// let pricePerItem = 2000;
+
+// productName = "Repair droid";
+// pricePerItem = pricePerItem += 1500;
+// console.log(productName); // Repair droid
+// console.log(pricePerItem); // 3500
+
+
+// blok - 1; task 5/36:
+// const productName = "Droid";
+// const pricePerItem = 3500;
+
+// // Change code below this line
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits.`
+// console.log(message); //You picked Droid, price per item is 3500 credits.
+
+// blok - 1; task 6/36:
