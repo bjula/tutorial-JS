4e7326c04decdfe13bd489dfd6336852f81c9126
Yuliia Besedina <92574244+bjula@users.noreply.github.com>
2022-01-31 20:28:15 +0200

    add task and example

--- js/script.js
+++ js/script.js
@@ -53,9 +53,81 @@
 // add(5, 10, 15);
 
 // blok - 1; task 9/36:
+// function add(a, b, c) {
+// return a + b + c;
+// }
+
+// add(2, 5, 8); // 15
+
+// console.log(add(15, 27, 10)); // 52
+// console.log(add(10, 20, 30)); //60
+// console.log(add(5, 10, 15)); // 30
+
+// // blok - 1; task 10/36:
+// function makeMessage (name, price) {
+//    const message = `You picked ${name}, price per item is ${price} credits`;
+//    console.log(message)
+//   return message;
+// };
+
+// // blok - 1; task 12/36:
+// function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+//     const message = `You ordered droids worth ${orderedQuantity * pricePerDroid + deliveryFee} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+//     console.log(message)
+//     return message;
+// };
+
+// // blok - 1; task 13/36:
+// function isAdult(age) {
+//   const passed = (age >= 18);
+//   console.log(passed);
+//   return passed;
+// }
+
+// blok - 1; task 14/36:
+// function isValidPassword(password) {
+//   const SAVED_PASSWORD = 'jqueryismyjam';
+//   const isMatch = (SAVED_PASSWORD === password);
+//   return isMatch;
+// };
+
+// blok - 1; task 15/36:
+// function checkAge(age) {
+//   let message;
+
+//   if (age >= 18) {
+//    message = 'You are an adult';
+//   } else {
+//     message = 'You are a minor';
+//   }
+//   console.log(message);
+//   return message;
+// }
 
 
+// Сложение чисел с плавающей точкой!!!!
+// Еще один способ - сложить, а результат отсечь до определённого знака после запятой при помощи метода toFixed().
 
+// console.log(0.17 + 0.24); // 0.41000000000000003
+// console.log((0.17 + 0.24).toFixed(2)); // 0.41
+
+
+// Методы строки toLowerCase() и toUpperCase():
+// console.log("saMsUng" === "samsung"); // false
+// console.log("saMsUng" === "SAMSUNG"); // false
+// // Чтобы не требовать абсолютно точный ввод можно сделать «нормализацию»
+// // введённой пользователем строки, то есть преобразовать все её символы в
+// // верхний или нижний регистр.Методы строки toLowerCase() и toUpperCase()
+// // вернут новую строку в соответствующем регистре, не изменяя оригинальную.
+
+// const BRAND_NAME = "SAMSUNG";
+// const userInput = "saMsUng";
+// const normalizedToUpperCaseInput = userInput.toUpperCase();
+
+// console.log(userInput); // 'saMsUng'
+// console.log(userInput === BRAND_NAME); // false
+// console.log(normalizedToUpperCaseInput); // 'SAMSUNG'
+// console.log(normalizedToUpperCaseInput === BRAND_NAME); // true
 
 
 
